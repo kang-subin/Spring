839b53616992e582c5c61e7dfb9494a02ca02ff8
강수빈 <tnbin98@gmail.com>
2022-10-25 15:21:10 +0900

    Callback

--- src/main/java/Dao/JdbcContext.java
+++ src/main/java/Dao/JdbcContext.java
@@ -10,7 +10,21 @@ public class JdbcContext {
     public JdbcContext(DataSource dataSource){ // 클래스에서 conneticon을 받은 후 서버랑 연결된 상태에서
         this.dataSource = dataSource;
     }
-    public void jdbcContextWithStatementStrategy(StatmentStrategy statmentStrategy) throws SQLException, ClassNotFoundException {
+
+    public void excutesql (String sql) throws SQLException, ClassNotFoundException {
+this.workWithStatmentStrategy(new StatmentStrategy() {
+    @Override // 익명클래스 (콜백)
+    public PreparedStatement makeStstment(Connection conn) throws SQLException {
+        return conn.prepareStatement(sql);
+    }
+});
+
+
+
+
+    }
+
+    public void workWithStatmentStrategy(StatmentStrategy statmentStrategy) throws SQLException, ClassNotFoundException {
         Connection conn = null;
         PreparedStatement ps = null;
 // 쿼리문을 작성하기 위해서 datasource를 받아 conn을 만들어서 쿼리문을 실행한다
--- src/main/java/Dao/UserDao.java
+++ src/main/java/Dao/UserDao.java
@@ -15,45 +15,46 @@ public class UserDao {
     UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-    UserDao(JdbcContext jdbcContext){
-    this.jdbcContext = new JdbcContext (dataSource);
-    }
+   UserDao(JdbcContext jdbcContext){
+        this.jdbcContext = new JdbcContext(dataSource);
+   }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        new JdbcContext(dataSource).jdbcContextWithStatementStrategy(new AddAllStrategy(user)); // add 쿼리 예외처리까지 된 상태의 메소드가 1줄로 완성
+       jdbcContext.workWithStatmentStrategy(new StatmentStrategy() {
+            @Override
+            public PreparedStatement makeStstment(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+                pstmt.setString(1, user.getId());
+                pstmt.setString(2, user.getName());
+                pstmt.setString(3, user.getPassword());
 
+                return conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+            } // 익명 클래스 닫기
+        });
     }
+     // add 쿼리 예외처리까지 된 상태
 
-    public User select(String id) throws SQLException, ClassNotFoundException {
-            Connection c = dataSource.getConnection();//connectionMaker.makeConnection();
-            PreparedStatement ps = c.prepareStatement("SELECT * FROM users where id =?");
-            ps.setString(1, id);
-            ResultSet rs = ps.executeQuery();
-
-
-            rs.next();
-            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-
-
-            rs.close();
-            ps.close();
-            c.close();
-
-            return user;
-    }
+//    public User select(String id) throws SQLException, ClassNotFoundException {
+//         jdbcContext.workWithStatmentStrategy(new StatmentStrategy() {
+//             @Override
+//             public PreparedStatement makeStstment(Connection conn) throws SQLException {
+//                 conn = .setString(1, id);
+//                 User user = new User(rs.getString("id"), rs.getString("name"), rs.getStrin
+//                 return null;
+//             }
+//         });
+//       g("password"));
+//            return user;
+//    }
 
     public void deleteAll() throws SQLException, ClassNotFoundException {
-       new JdbcContext(dataSource).jdbcContextWithStatementStrategy(new DeleteAlllStrategy());
-    }
-
-//        public int getCount () throws SQLException {
-//
-//        }
+        jdbcContext.excutesql("delete from users");}
 
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         UserDao userDao = new UserDao(new UserdaoFactory().awsdataSource());
+        userDao.deleteAll();
         userDao.add(new User("1", "ss" ,"1234"));
 
 
